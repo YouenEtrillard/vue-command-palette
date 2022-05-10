a7ca487ec3f08ffe93078e53f3acb6947cd2268b
YouenEtrillard <contact@youen-etrillard.com>
2022-05-10 18:20:16 +0200

    feature: add todo and commands page

--- src/pages/Commands.vue
+++ src/pages/Commands.vue
@@ -1,9 +1,28 @@
+<script setup lang="ts">
+import { useCommandStore } from '../stores/CommandStore';
+
+const commandStore = useCommandStore();
+</script>
+
 <template>
-  <div>
-    <h1>Commands</h1>
-  </div>
+  <h1>Commands</h1>
+  <ul>
+    <li v-for="(command, index) in commandStore.commandList" :key="command.id">
+      {{ command.title }}
+      <div>
+        <input type="text" name="" id="" />
+        <button>Add alias</button>
+      </div>
+      <ul>
+        <li>Alias #1</li>
+        <li>Alias #2</li>
+      </ul>
+    </li>
+  </ul>
 </template>
 
-<script setup lang="ts"></script>
-
-<style scoped></style>
+<style scoped>
+li {
+  text-align-last: left;
+}
+</style>
--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import Counter from '../components/Counter.vue';
+import Todo from '../components/Todo.vue';
 </script>
 
 <template>
   <div>
-    <h1>Home page</h1>
-
-    <Counter />
+    <h1>Todo</h1>
+    <Todo />
   </div>
 </template>
 
