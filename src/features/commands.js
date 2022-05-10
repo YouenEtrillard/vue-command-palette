9aae5588c08c86931424b9ce9c6cac06bb67b030
YouenEtrillard <contact@youen-etrillard.com>
2022-05-10 18:19:48 +0200

    feature: add uuid to commands

--- src/features/commands.js
+++ src/features/commands.js
@@ -1,10 +1,11 @@
+import { v4 as uuidv4 } from 'uuid';
 import { useCommandStore } from '../stores/CommandStore';
 
 export const register = (config) => {
   const commandStore = useCommandStore();
 
   commandStore.commandList.push({
-    id: 356,
+    id: uuidv4(),
     title: config.name,
     command: config.function,
     hotkeys: config.hotkeys || [],
