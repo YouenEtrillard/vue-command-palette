7c9c955be4363790cc1639a02f9189449259bf29
YouenEtrillard <contact@youen-etrillard.com>
2022-05-10 12:50:18 +0200

    register additional commands

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -5,17 +5,25 @@ import { register as registerCommand } from '../features/commands';
 const currentCount = ref(0);
 
 const incrementCount = () => currentCount.value++;
+const decrementCount = () => currentCount.value--;
 
 registerCommand({
   name: 'Increment count',
   function: incrementCount,
   hotkeys: ['Ctrl_Alt_I'],
 });
+
+registerCommand({
+  name: 'Decrement count',
+  function: decrementCount,
+  hotkeys: ['Ctrl_Alt_D'],
+});
 </script>
 
 <template>
   <h2>Counter</h2>
   <p>{{ currentCount }}</p>
+  <button @click="decrementCount">-</button>
   <button @click="incrementCount">+</button>
 </template>
 
---
+++ src/components/Todo.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { register as registerCommand } from '../features/commands';
+
+const taskList = ref([
+  {
+    name: 'static task',
+  },
+]);
+
+const addTask = () => {
+  taskList.value.push({
+    name: 'added task',
+  });
+};
+
+registerCommand({
+  name: 'Add task to todo list',
+  function: addTask,
+});
+</script>
+
+<template>
+  <ul>
+    <li v-for="(task, index) in taskList" :key="index">
+      {{ task.name }}
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
